8213dd4f00dbebc89d9f8f34e2e9612753c59516
sunilbansod <sunilbansod@outlook.com>
2025-10-04 15:05:14 +0530

    Fix region to ap-south-1

--- prod-infra/main.tf
+++ prod-infra/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
-  availability_zones   = ["us-east-1a", "us-east-1b"]
+  availability_zones   = ["ap-south-1a", "ap-south-1b"]
   enable_nat_gateway   = true
 
   tags = {

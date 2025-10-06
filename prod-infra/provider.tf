d3a5d5bc66be7fb39aa1a4aa5c58de97ba12f594
sunilbansod <sunilbansod@outlook.com>
2025-10-06 17:44:41 +0530

    Terrafrom format corrected

--- prod-infra/provider.tf
+++ prod-infra/provider.tf
@@ -9,10 +9,10 @@ terraform {
 
   # Backend configuration for state management in s3 bucket
   backend "s3" {
-    bucket         = "sunil-git-my-terraform-state-bucket"
-    key            = "prod-infra-terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
+    bucket  = "sunil-git-my-terraform-state-bucket"
+    key     = "prod-infra-terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
     #use_lockfile   = true
     #dynamodb_table = "terraform-lock-table"  # This DynamoDB table must exist in your AWS account
   }
